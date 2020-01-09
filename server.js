2bdfeb7445480a7974427b6498cd39604b7ea06a
Alec <alec007@t-online.de>
2020-01-09 13:01:29 +0100

    fixed: no data in /api/users

--- server.js
+++ server.js
@@ -21,19 +21,18 @@ app.get("/api/users", (req, res, next) => {
     var sql = "select * from user"
     var params = []
     db.all(sql, params, (err, rows) => {
-        if(err) {
-            res.status(400).json({"error":err.message})
-            return
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
         }
         res.json({
             "message":"success",
             "data":rows
         })
-    });
+      });
 });
 
-
-// DEfault response for any other request
+// Default response for any other request
 app.use(function(req, res) {
     res.status(404)
 });
