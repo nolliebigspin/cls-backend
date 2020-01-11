1d19a64c4e252186bd45798dfd43d7b45e2c7e6c
nolliebigpsin <alec007@t-online.de>
2020-01-11 16:02:20 +0100

    added POST userData

--- server.js
+++ server.js
@@ -107,6 +107,42 @@ app.get("/api/userData", (req, res, next) => {
     });
 });
 
+app.post("/api/userData", (req, res, next) => {
+    var data = {
+        id: req.body.id,
+        firstUsed: req.body.firstUsed,
+        useTime: req.body.useTime,
+        gender: req.body.gender,
+        age: req.body.age,
+        final_Difficulty: req.body.final_Difficulty,
+        time: req.body.time,
+        groupID: req.body.gropID,
+        textediting: req.body.textediting,
+        spreadsheets: req.body.spreadsheets,
+        presentation: req.body.presentation,
+        pictureediting: req.body.pictureediting,
+        coding: req.body.coding,
+        gaming: req.body.gaming,
+        email: req.body.email,
+        internetsurfing: req.body.internetsurfing,
+        informationgathering: req.body.informationgathering,
+        onlineshopping: req.body.onlineshopping,
+        onlinebanking: req.body.onlineshopping 
+    }
+    var sql = "INSERT INTO userData (firstUsed, useTime, gender, age, final_Difficulty, time, groupID, textediting, spreadsheets, presentation, pictureediting, coding, gaming, email, internetsurfing, informationgathering, onlineshopping, onlinebanking) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
+    var params = [data.id, data.firstUsed, data.useTime, data.gender, data.age, data.final_Difficulty, data.time, data.groupID, data.textediting, data.spreadsheets, data.presentation, data.pictureediting, data.coding, data.gaming, data.email, data.internetsurfing, data.informationgathering, data.onlineshopping, data.onlinebanking]
+    db.run(sql, params, function(err, result) {
+        if(err) {
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "data": data,
+            "id": this.lastID
+        })
+    });
+});
+
 // Default response for any other request
 app.use(function(req, res) {
     res.status(404)
